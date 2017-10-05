7c0cb9512180b0d2a50824eb9cf8b77e037cfd74
Darcy <qaz4635888@gmail.com>
2017-10-05 12:38:12 +0800

    name and age

--- client/main.js
+++ client/main.js
@@ -1,22 +1,99 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+/*var HelloMessage ="hellomessage";
+ console.log(HelloMessage);
+HelloMessage = "hey alyssa";
+console.log(HelloMessage);
 
-import './main.html';
+var fruit = "apple";
+var applenum = 3;
+console.log(fruit);
+console.log(applenum);
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+var 你是我的小老鼠 = 100 ;
+console.log(你是我的小老鼠);
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+var mouse = 5, dog = 20;
+var allanimals = mouse+dog;
+console.log(allanimals);
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+var question = "where is your dog?", answer="No";
+console.log(question+answer);
+
+var numbermessage="totalnumber is   ", totalnumber= 10;
+console.log(numbermessage+totalnumber);
+
+var numberone=10, numbertwo=20;
+console.log(numberone+numbertwo);
+
+var person ="Darcy";
+if (person ==="Darcy"  )
+{
+  console.log("helloooooooooooooooo");
+}
+else
+{
+  console.log ("you are not Darcy");
+}
+
+var IQ =180;
+if(IQ>=180)
+{
+  console.log("u r so smart");
+}
+else
+{
+  console.log("me too");
+}
+
+var language = "taiwanese";
+if(language==="English")
+{
+  console.log("hi");
+}
+else if (language === "taiwanese")
+{
+  console.log(""哩後"");
+}
+else
+{
+  console.log("con ni gi wa");
+}
+
+
+for(age=0; age<100; age++)
+{
+  if(age===0)
+  {
+    console.log("hello");
+  }
+  else if(age===25)
+  {
+    console.log("you r so beautiful");
+  }
+  else if(age===75)
+  {
+    console.log("it's time to go hell");
+  }
+}*/
+
+
+var nameone = "jason", ageone = 18;
+var nametwo =  "ben", agetwo = 40;
+
+var agecomment = function(name, age)
+{
+  var msg;
+  if(age <= 20 )
+  {
+  msg = name+", u r so young!";
+  }
+  else
+   {
+    msg = name+", u r an adult.";
+  }
+  return msg;
+}
+
+var comment = agecomment(nameone, ageone);
+console.log(comment);
+comment = agecomment(nametwo, agetwo);
+console.log(comment);
